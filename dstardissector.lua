5d5fe97e70fced8b3f32b41d1d61b59e189f42cd
phl0 <florian@florian-wolters.de>
2016-04-24 17:23:12 +0200

    Add basics for voice frames

--- dstardissector.lua
+++ dstardissector.lua
@@ -71,34 +71,39 @@ function dstar_proto.dissector(buffer,pinfo,tree)
        local dstar_frame_counter = buffer(14, 1)
        dstar_tree:add(dstar_frame_counter, "Frame Counter: " .. dstar_frame_counter:uint() .. " (0x" .. dstar_frame_counter .. ")")
 
-       local dstar_flags = buffer(15, 3)
-
        if frame_type == "Configuration Frame" then
+          local dstar_flags = buffer(15, 3)
+
           local dstar_destination_repeater = buffer(18, 8)
           local destination = tostring(dstar_destination_repeater)
-
           dstar_tree:add(dstar_destination_repeater, "Destination Repeater: " .. string.fromhex(destination))
 
           local dstar_source_repeater = buffer(26, 8)
           local source = tostring(dstar_source_repeater)
-
           dstar_tree:add(dstar_source_repeater, "Source Repeater: " .. string.fromhex(source))
 
           local dstar_your_call = buffer(34, 8)
           local urcall = tostring(dstar_your_call)
-
           dstar_tree:add(dstar_your_call, "Your Call: " .. string.fromhex(urcall))
 
           local dstar_my_call = buffer(42, 8)
           local mycall = tostring(dstar_my_call)
-
           local dstar_my_suffix = buffer(50, 4)
           local mysuffix = tostring(dstar_my_suffix)
-
           dstar_tree:add(buffer(42, 12), "My Call: " .. string.fromhex(mycall) .. "/" .. string.fromhex(mysuffix))
 
           local dstar_checksum = buffer(54, 2)
           dstar_tree:add(dstar_checksum, "Checksum: 0x" .. dstar_checksum)
+
+       elseif frame_type == "Voice Frame" then
+          local dstar_ambe_data = buffer(15, 9)
+          local ambe_data = tostring(dstar_ambe_data)
+          dstar_tree:add(dstar_ambe_data, "AMBE Data: " .. ambe_data)
+
+          local dstar_slow_data = buffer(24,3)
+          local slow_data = tostring(dstar_slow_data)
+          dstar_tree:add(dstar_slow_data, "Slow Data: " .. slow_data)
+
        end
 
 
