a27235b9f9a6e37f567d7fc9c4aec5a6cc74f248
phl0 <florian@florian-wolters.de>
2016-04-24 16:49:25 +0200

    Correct indentiation

--- dstardissector.lua
+++ dstardissector.lua
@@ -72,33 +72,33 @@ function dstar_proto.dissector(buffer,pinfo,tree)
        dstar_tree:add(dstar_frame_counter, "Frame Counter: " .. dstar_frame_counter:uint() .. " (0x" .. dstar_frame_counter .. ")")
 
        local dstar_flags = buffer(15, 3)
-      
+
        if frame_type == "Configuration Frame" then
-       local dstar_destination_repeater = buffer(18, 8)
-       local destination = tostring(dstar_destination_repeater)
+          local dstar_destination_repeater = buffer(18, 8)
+          local destination = tostring(dstar_destination_repeater)
 
-       dstar_tree:add(dstar_destination_repeater, "Destination Repeater: " .. string.fromhex(destination))
+          dstar_tree:add(dstar_destination_repeater, "Destination Repeater: " .. string.fromhex(destination))
 
-       local dstar_source_repeater = buffer(26, 8)
-       local source = tostring(dstar_source_repeater)
+          local dstar_source_repeater = buffer(26, 8)
+          local source = tostring(dstar_source_repeater)
 
-       dstar_tree:add(dstar_source_repeater, "Source Repeater: " .. string.fromhex(source))
+          dstar_tree:add(dstar_source_repeater, "Source Repeater: " .. string.fromhex(source))
 
-       local dstar_your_call = buffer(34, 8)
-       local urcall = tostring(dstar_your_call)
+          local dstar_your_call = buffer(34, 8)
+          local urcall = tostring(dstar_your_call)
 
-       dstar_tree:add(dstar_your_call, "Your Call: " .. string.fromhex(urcall))
+          dstar_tree:add(dstar_your_call, "Your Call: " .. string.fromhex(urcall))
 
-       local dstar_my_call = buffer(42, 8)
-       local mycall = tostring(dstar_my_call)
+          local dstar_my_call = buffer(42, 8)
+          local mycall = tostring(dstar_my_call)
 
-       local dstar_my_suffix = buffer(50, 4)
-       local mysuffix = tostring(dstar_my_suffix)
+          local dstar_my_suffix = buffer(50, 4)
+          local mysuffix = tostring(dstar_my_suffix)
 
-       dstar_tree:add(buffer(42, 12), "My Call: " .. string.fromhex(mycall) .. "/" .. string.fromhex(mysuffix))
+          dstar_tree:add(buffer(42, 12), "My Call: " .. string.fromhex(mycall) .. "/" .. string.fromhex(mysuffix))
 
-       local dstar_checksum = buffer(54, 2)
-       dstar_tree:add(dstar_checksum, "Checksum: 0x" .. dstar_checksum)
+          local dstar_checksum = buffer(54, 2)
+          dstar_tree:add(dstar_checksum, "Checksum: 0x" .. dstar_checksum)
        end
 
 
